557db957f7592d94f4b82f4f82f6e1c4e98f05f2
Zachary Crumbo <zcrumbo@mac.com>
2018-06-10 18:01:46 -0700

    clean up test data

--- components/chart/chart.component.js
+++ components/chart/chart.component.js
@@ -7,44 +7,35 @@ export class Chart extends Component {
     this.state = {};
   }
   render() {
+    // TODO replace test data with real data
     const data = [
       {
         month: new Date(2015, 0, 1),
-        apples: 3840,
-        bananas: 1920,
-        cherries: 960,
-        dates: 400,
+        generated: 3840,
+        consumed: 1920,
       },
       {
         month: new Date(2015, 1, 1),
-        apples: 1600,
-        bananas: 1440,
-        cherries: 960,
-        dates: 400,
+        generated: 1600,
+        consumed: 1440,
       },
       {
         month: new Date(2015, 2, 1),
-        apples: 640,
-        bananas: 960,
-        cherries: 3640,
-        dates: 400,
+        generated: 640,
+        consumed: 960,
       },
       {
         month: new Date(2015, 3, 1),
-        apples: 3320,
-        bananas: 480,
-        cherries: 640,
-        dates: 400,
+        generated: 3320,
+        consumed: 480,
       },
     ];
 
-    const colors = ['#8800cc', '#aa00ff', '#cc66ff', '#eeccff'];
-    const keys = ['apples', 'bananas', 'cherries', 'dates'];
+    const colors = ['#8800cc', '#aa00ff'];
+    const keys = ['generated', 'consumed'];
     const svgs = [
-      { onPress: () => console.log('apples') },
-      { onPress: () => console.log('bananas') },
-      { onPress: () => console.log('cherries') },
-      { onPress: () => console.log('dates') },
+      { onPress: () => console.log('generated') },
+      { onPress: () => console.log('consumed') },
     ];
 
     return (
